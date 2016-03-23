feb30eec4b30bf6f5b3b79743527c940861bb179
mike ritter <ritterml@yahoo.com>
2016-03-23 15:11:02 -0400

    initial commit

---
+++ ritterml-ministry-manager.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: Ministry Manager
+ */
+
+/**
+ * Register Activation Hook.
+ */
+function rmm_activate(){}
+
+/**
+ * Register Deactivation Hook.
+ */
+function rmm_deactivate(){}
+
+/**
+ * Register Uninstall Hook.
+ */
+function rmm_uninstall(){}
